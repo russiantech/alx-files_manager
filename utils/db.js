da3cbfcbdd14cbf87d85214d306626cf8103fc2b
russiantech <chrisjsmez@gmail.com>
2024-06-27 22:29:39 -0700

    redis & mongodb set

---
+++ utils/db.js
@@ -0,0 +1,31 @@
+// utils/db.js
+
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+
+    this.client = new MongoClient(`mongodb://${host}:${port}`, { useUnifiedTopology: true });
+    this.client.connect().catch((err) => console.error('MongoDB Client Error', err));
+    this.db = this.client.db(database);
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
+
---
+++ utils/redis.js
@@ -0,0 +1,44 @@
+// utils/redis.js
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => console.error('Redis Client Error', err));
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, value) => {
+        if (err) return reject(err);
+        resolve(value);
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (err) => {
+        if (err) return reject(err);
+        resolve();
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err) => {
+        if (err) return reject(err);
+        resolve();
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
+
